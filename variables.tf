a77b8a36a936dd62a944610cf3f8f6108dc09011
HariDEV-GIT <78740469+HariDEV-GIT@users.noreply.github.com>
2022-02-20 12:37:09 +0530

    Update variables.tf
    change in sg

--- variables.tf
+++ variables.tf
@@ -10,7 +10,7 @@ variable "instance_type" {
 
 variable "security_group" {
   description = "Name of security group"
-  default     = "my-jenkins-sg-prod"
+  default     = "prod-sg"
 }
 
 variable "tag_name" {
