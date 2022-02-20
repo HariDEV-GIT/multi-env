e59c5879e8115cc7ac73cfa480074fb37d4d6e76
HariDEV-GIT <78740469+HariDEV-GIT@users.noreply.github.com>
2022-02-20 12:09:15 +0530

    Create variables.tf
    variables added

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "aws_region" {
+  description = "The AWS region to create things in."
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "instance type for ec2"
+  default     = "t2.micro"
+}
+
+variable "security_group" {
+  description = "Name of security group"
+  default     = "my-jenkins-sg"
+}
+
+variable "tag_name" {
+  description = "Tag Name of for Ec2 instance"
+  default     = "My-EC2"
+}
+
+variable "ami_id" {
+  description = "AMI for Ubuntu Ec2 instance"
+  default     = "ami-0ed9277fb7eb570c9"
+}
